eebb586fd2a9eaae90842489e2654cdd2daa42f6
Ács Dániel <acsi@444.hu>
2014-08-27 13:37:12 +0200

    Update distribution files

--- scribe-plugin-forecolor-command.js
+++ scribe-plugin-forecolor-command.js
@@ -1,50 +1,45 @@
-define("scribe-plugin-forecolor-command",
-  ["exports"],
-  function(__exports__) {
-    "use strict";
-    __exports__["default"] = function() {
-        return function(scribe) {
-            var foreColorCommand = new scribe.api.Command('foreColor');
-
-            var clean = function(root) {
-                for (var i=0; i < root.childNodes.length; i++) {
-                    var node = root.childNodes[i];
-                    if (node.nodeType !== 1) {
-                        continue;
-                    }
-
-                    node.style.color = "";
-                    clean(node);
-
-                    if (node.attributes.length == 1 && !node.getAttribute('style')) {
-                        while (node.childNodes.length > 0) {
-                            root.insertBefore(node.firstChild, node);
-                        }
-                        root.removeChild(node);
-                        i--;
-                    }
-                }
-
-                root.normalize();
-            };
-
-            foreColorCommand.execute = function(color) {
-                var selection = new scribe.api.Selection();
-                var range = selection.range;
-                var rangeContent = range.extractContents();
-
-                var wrapper = document.createElement("span");
-                wrapper.style.color = color;
-
-                scribe.transactionManager.run(function () {
-                    clean(rangeContent);
-
-                    range.insertNode(rangeContent);
-                    range.surroundContents(wrapper);
-                }.bind(this));
-            };
-
-            scribe.commands.foreColor = foreColorCommand;
-        };
-    }
-  });
+define("scribe-plugin-forecolor-command", ['scribe-common/src/element'], function($__0) {
+  "use strict";
+  var __moduleName = "scribe-plugin-forecolor-command";
+  if (!$__0 || !$__0.__esModule)
+    $__0 = {default: $__0};
+  var Element = $__0.default;
+  var $__default = function() {
+    return function(scribe) {
+      var foreColorCommand = new scribe.api.Command('foreColor');
+      var clean = function(root) {
+        for (var i = 0; i < root.childNodes.length; i++) {
+          var node = root.childNodes[i];
+          if (node.nodeType !== 1) {
+            continue;
+          }
+          node.style.color = "";
+          clean(node);
+          if (node.attributes.length == 1 && !node.getAttribute('style')) {
+            Element.unwrap(root, node);
+            i--;
+          }
+        }
+      };
+      foreColorCommand.execute = function(color) {
+        var selection = new scribe.api.Selection();
+        var range = selection.range;
+        var rangeContent = range.extractContents();
+        var wrapper = document.createElement("span");
+        wrapper.style.color = color;
+        scribe.transactionManager.run(function() {
+          clean(rangeContent);
+          range.insertNode(rangeContent);
+          range.surroundContents(wrapper);
+        }.bind(this));
+      };
+      scribe.commands.foreColor = foreColorCommand;
+    };
+  };
+  return {
+    get default() {
+      return $__default;
+    },
+    __esModule: true
+  };
+});
